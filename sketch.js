22b78600d8efec4a61c8535d518c9b0a67b3c30f
ZeneS13 <sangoi.zene@gmail.com>
2020-05-23 11:01:46 +0530

    done

--- sketch.js
+++ sketch.js
@@ -1,29 +1,39 @@
 var ball;
-
+var database;
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    database=firebase.database();
+    var ref=database.ref('ball/position');
+    ref.on("value",readPos);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x1,y1){
+   database.ref('ball/position').set({
+     x:ball.x+x1,
+     y:ball.y+y1  
+   });
+}
+function readPos(data){
+var position=data.val();
+ball.x = position.x;
+ball.y=position.y;
 }
